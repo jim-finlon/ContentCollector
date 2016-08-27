1b1c571fd54521ad43afd8db67226be7e3cc3213
jim-finlon <jim@finlon.net>
2016-08-27 16:10:13 -0600

    DataManager built

--- ContentCollector/ContentCollector/DataManager.cs
+++ ContentCollector/ContentCollector/DataManager.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using ContentCollector.Model;
 using Microsoft.WindowsAzure.MobileServices;
 using Microsoft.WindowsAzure.MobileServices.SQLiteStore;
+using Microsoft.WindowsAzure.MobileServices.Sync;
 
 namespace ContentCollector
 {
@@ -13,7 +14,13 @@ namespace ContentCollector
     {
         private readonly MobileServiceClient _client;
 
-        private readonly IMobileServiceTable<Location> _locationTable;
+        private readonly IMobileServiceSyncTable<Location> _locationTable;
+        public static DataManager DefaultManager { get; private set; } = new DataManager();
+
+        public MobileServiceClient CurrentClient
+        {
+            get { return _client; }
+        }
 
         public DataManager()
         {
@@ -24,11 +31,98 @@ namespace ContentCollector
                 var store = new MobileServiceSQLiteStore("localstore.db");
                 store.DefineTable<Location>();
                 _client.SyncContext.InitializeAsync(store);
+
+                _locationTable = _client.GetSyncTable<Location>();
+
+                _locationTable.PullAsync(null, _locationTable.CreateQuery());
             }
-            catch (Exception)
+            catch (Exception ex)
             {
-                
+                Debug.WriteLine(ex.Message);
                 throw;
+            }
+        }
+
+        public async Task<ObservableCollection<Location>> GetLocationsAsync(bool syncItems = false)
+        {
+            try
+            {
+
+                if (syncItems)
+                {
+                    await SyncAsync();
+                }
+
+                IEnumerable<Location> locations = await _locationTable.ToEnumerableAsync();
+
+                return new ObservableCollection<Location>(locations);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(@"Get Error: {0}", ex.Message);
+                throw;
+
+            }
+        }
+
+        public async Task SaveLocationAsync(Location item)
+        {
+            try
+            {
+                if (item.Id == null)
+                {
+                    await _locationTable.InsertAsync(item);
+                }
+                else
+                {
+                    await _locationTable.UpdateAsync(item);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(@"Save Error: {0}", ex.Message);
+                throw;
+            }
+        }
+
+        private async Task SyncAsync()
+        {
+            ReadOnlyCollection<MobileServiceTableOperationError> syncErrors = null;
+            try
+            {
+                await _client.SyncContext.PushAsync();
+                await _locationTable.PullAsync("alllocations", _locationTable.CreateQuery());
+            }
+            catch (MobileServicePushFailedException exc)
+            {
+                if (exc.PushResult != null)
+                {
+                    syncErrors = exc.PushResult.Errors;
+                }
+            }
+
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                throw;
+            }
+
+            if (syncErrors != null)
+            {
+                foreach (var error in syncErrors)
+                {
+                    if (error.OperationKind == MobileServiceTableOperationKind.Update && error.Result != null)
+                    {
+                        await error.CancelAndUpdateItemAsync(error.Result);
+                    }
+                    else
+                    {
+                        await error.CancelAndDiscardItemAsync();
+                    }
+
+                    Debug.WriteLine(@"Error executing sync opertation. Item: {0} ({1}). Operation discarded.", error.TableName, error.Item["id"]);
+                }
             }
         }
     }
